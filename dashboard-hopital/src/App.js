64b282b68dc49cf6a424aa65005de6fbba37eccc
SafiMoustapha <157801425+SafiMoustapha@users.noreply.github.com>
2025-03-13 09:43:52 +0100

    modif

--- dashboard-hopital/src/App.js
+++ dashboard-hopital/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import HomePage from "./components/HomePage"; // Si tu as une page d'accueil
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} /> {/* Page d'accueil */}
+        <Route path="/dashboard" element={<Dashboard />} /> {/* Tableau de bord */}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}","./*.html"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
